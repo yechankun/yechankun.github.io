b08e6c81627bc9c8323da1046230dbe18a3698f3
yechankun <redniche@naver.com>
2023-04-23 22:51:40 +0900

    Update jekyll javascript
    블로그에 적용된 javascript의 일부 구현 변경
    1. back-to-top 조건 변경: 기존에 모바일 환경에선 back-to-top이 나오지 않는 조건을 삭제
    2. js파일 패키징 변경, home에서도 이제 이미지를 lazy 로딩하게 변경

--- _javascript/commons/back-to-top.js
+++ _javascript/commons/back-to-top.js
@@ -1,10 +1,9 @@
 /*
 Reference: https://bootsnipp.com/snippets/featured/link-to-top-page
 */
-$(function() {
+$(function () {
   $(window).scroll(() => {
-    if ($(this).scrollTop() > 50 &&
-      $("#sidebar-trigger").css("display") === "none") {
+    if ($(this).scrollTop() > 50) {
       $("#back-to-top").fadeIn();
     } else {
       $("#back-to-top").fadeOut();
@@ -12,9 +11,12 @@ $(function() {
   });
 
   $("#back-to-top").click(() => {
-    $("body,html").animate({
-      scrollTop: 0
-    }, 800);
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      800
+    );
     return false;
   });
 });
--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -2,101 +2,107 @@
 
 "use strict";
 
-const { src, dest, watch, series, parallel} = require('gulp');
+const { src, dest, watch, series, parallel } = require("gulp");
 
-const concat = require('gulp-concat');
+const concat = require("gulp-concat");
 const rename = require("gulp-rename");
-const uglify = require('gulp-uglify');
-const insert = require('gulp-insert');
-const fs = require('fs');
+const uglify = require("gulp-uglify");
+const insert = require("gulp-insert");
+const fs = require("fs");
 
-const JS_SRC = '_javascript';
+const JS_SRC = "_javascript";
 const JS_DEST = `assets/js/dist`;
 
 function concatJs(files, output) {
   return src(files)
     .pipe(concat(output))
-    .pipe(rename({ extname: '.min.js' }))
+    .pipe(rename({ extname: ".min.js" }))
     .pipe(dest(JS_DEST));
 }
 
 function minifyJs() {
-  return src(`${ JS_DEST }/*.js`)
-    .pipe(insert.prepend(fs.readFileSync(`${ JS_SRC }/copyright`, 'utf8')))
-    .pipe(uglify({output: {comments: /^!|@preserve|@license|@cc_on/i}}))
+  return src(`${JS_DEST}/*.js`)
+    .pipe(insert.prepend(fs.readFileSync(`${JS_SRC}/copyright`, "utf8")))
+    .pipe(uglify({ output: { comments: /^!|@preserve|@license|@cc_on/i } }))
     .pipe(dest(JS_DEST));
 }
 
 const commonsJs = () => {
-  return concatJs(`${JS_SRC}/commons/*.js`, 'commons');
+  return concatJs(`${JS_SRC}/commons/*.js`, "commons");
 };
 
 const homeJs = () => {
-  return concatJs([
+  return concatJs(
+    [
       `${JS_SRC}/commons/*.js`,
-      `${JS_SRC}/utils/timeago.js`
+      `${JS_SRC}/utils/timeago.js`,
+      `${JS_SRC}/utils/img-extra.js`,
     ],
-    'home'
+    "home"
   );
 };
 
 const postJs = () => {
-  return concatJs([
+  return concatJs(
+    [
       `${JS_SRC}/commons/*.js`,
       `${JS_SRC}/utils/img-extra.js`,
       `${JS_SRC}/utils/timeago.js`,
       `${JS_SRC}/utils/checkbox.js`,
       `${JS_SRC}/utils/clipboard.js`,
       // 'smooth-scroll.js' must be called after ToC is ready
-      `${JS_SRC}/utils/smooth-scroll.js`
-    ], 'post'
+      `${JS_SRC}/utils/smooth-scroll.js`,
+    ],
+    "post"
   );
 };
 
 const categoriesJs = () => {
-  return concatJs([
-      `${JS_SRC}/commons/*.js`,
-      `${JS_SRC}/utils/category-collapse.js`
-    ], 'categories'
+  return concatJs(
+    [`${JS_SRC}/commons/*.js`, `${JS_SRC}/utils/category-collapse.js`],
+    "categories"
   );
 };
 
 const pageJs = () => {
-  return concatJs([
+  return concatJs(
+    [
       `${JS_SRC}/commons/*.js`,
       `${JS_SRC}/utils/checkbox.js`,
       `${JS_SRC}/utils/img-extra.js`,
       `${JS_SRC}/utils/clipboard.js`,
-      `${JS_SRC}/utils/smooth-scroll.js`
-    ], 'page'
+      `${JS_SRC}/utils/smooth-scroll.js`,
+    ],
+    "page"
   );
 };
 
 const miscJs = () => {
-  return concatJs([
-      `${JS_SRC}/commons/*.js`,
-      `${JS_SRC}/utils/locale-datetime.js`
-    ], 'misc'
+  return concatJs(
+    [`${JS_SRC}/commons/*.js`, `${JS_SRC}/utils/locale-datetime.js`],
+    "misc"
   );
 };
 
 // GA pageviews report
 const pvreportJs = () => {
-  return concatJs(`${JS_SRC}/utils/pageviews.js`, 'pvreport');
+  return concatJs(`${JS_SRC}/utils/pageviews.js`, "pvreport");
 };
 
 const buildJs = parallel(
-  commonsJs, homeJs, postJs, categoriesJs, pageJs, miscJs, pvreportJs);
+  commonsJs,
+  homeJs,
+  postJs,
+  categoriesJs,
+  pageJs,
+  miscJs,
+  pvreportJs
+);
 
 exports.build = series(buildJs, minifyJs);
 
 exports.liveRebuild = () => {
   buildJs();
 
-  watch([
-      `${ JS_SRC }/commons/*.js`,
-      `${ JS_SRC }/utils/*.js`
-    ],
-    buildJs
-  );
+  watch([`${JS_SRC}/commons/*.js`, `${JS_SRC}/utils/*.js`], buildJs);
 };
